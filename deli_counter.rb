bd689522b0de14b5c1a4f463bbe74d6454875473
A Belgrave <abelgrave95@gmail.com>
2019-02-25 17:10:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def line(ary)
+  result = "The line is currently: "
+  if ary.size == 0
+    "The line is currently empty."
+  else
+    ary.each_index { |ind| result << "#{ind+1}. #{arr[ind]} " }
+  end
+end
