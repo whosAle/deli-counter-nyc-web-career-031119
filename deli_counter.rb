fbbe5656b9812a352fb1b4fd30e68822245e09fd
A Belgrave <abelgrave95@gmail.com>
2019-02-25 17:20:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ def now_serving(line)
   if line.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line.pop}"
+    puts "Currently serving #{line.shift}"
   end
 end
